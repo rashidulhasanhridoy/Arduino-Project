ff7663f72ddb2ed81d0a9fd3dba72c8d3deb822f
Rashidul Hasan Hridoy <63078167+rashidulhasanhridoy@users.noreply.github.com>
2020-07-26 02:11:35 +0600

    Create LightPlay.ino

---
+++ LightPlay.ino
@@ -0,0 +1,229 @@
+int redLed = 7;
+int greenLed = 6;
+int yellowLed = 5;
+int whiteLed = 4;
+int red2led = 3;
+
+void setup() {
+// put your setup code here, to run once:
+pinMode(7, OUTPUT);
+pinMode(6, OUTPUT);
+pinMode(5, OUTPUT);
+pinMode(4, OUTPUT);
+pinMode(3, OUTPUT);
+}
+
+void loop() {
+// put your main code here, to run repeatedly:
+digitalWrite(7, HIGH);
+delay(100);
+digitalWrite(7, LOW);
+delay(100);
+digitalWrite(6, HIGH);
+delay(100);
+digitalWrite(6, LOW);
+delay(100);
+digitalWrite(5, HIGH);
+delay(100);
+digitalWrite(5, LOW);
+delay(100);
+digitalWrite(4, HIGH);
+delay(100);
+digitalWrite(4, LOW);
+delay(100);
+digitalWrite(3, HIGH);
+delay(100);
+digitalWrite(3, LOW);
+delay(100);
+
+
+digitalWrite(3, HIGH);
+delay(100);
+digitalWrite(3, LOW);
+delay(100);
+digitalWrite(4, HIGH);
+delay(100);
+digitalWrite(4, LOW);
+delay(100);
+digitalWrite(5, HIGH);
+delay(100);
+digitalWrite(5, LOW);
+delay(100);
+digitalWrite(6, HIGH);
+delay(100);
+digitalWrite(6, LOW);
+delay(100);
+digitalWrite(7, HIGH);
+delay(100);
+digitalWrite(7, LOW);
+delay(100);
+
+digitalWrite(7, HIGH);
+delay(300);
+digitalWrite(7, LOW);
+delay(300);
+digitalWrite(3, HIGH);
+delay(300);
+digitalWrite(3, LOW);
+delay(300);
+digitalWrite(3, HIGH);
+delay(300);
+digitalWrite(3, LOW);
+delay(300);
+digitalWrite(7, HIGH);
+delay(300);
+digitalWrite(7, LOW);
+delay(300);
+
+digitalWrite(6, HIGH);
+delay(300);
+digitalWrite(6, LOW);
+delay(300);
+digitalWrite(4, HIGH);
+delay(300);
+digitalWrite(4, LOW);
+delay(300);
+digitalWrite(4, HIGH);
+delay(300);
+digitalWrite(4, LOW);
+delay(300);
+digitalWrite(6, HIGH);
+delay(300);
+digitalWrite(6, LOW);
+delay(300);
+
+
+digitalWrite(5, HIGH);
+delay(100);
+digitalWrite(5, LOW);
+delay(100);
+digitalWrite(5, HIGH);
+delay(100);
+digitalWrite(5, LOW);
+delay(100);
+digitalWrite(5, HIGH);
+delay(100);
+digitalWrite(5, LOW);
+delay(100);
+
+digitalWrite(6, HIGH);
+delay(300);
+digitalWrite(6, LOW);
+delay(300);
+digitalWrite(4, HIGH);
+delay(300);
+digitalWrite(4, LOW);
+delay(300);
+digitalWrite(4, HIGH);
+delay(300);
+digitalWrite(4, LOW);
+delay(300);
+digitalWrite(6, HIGH);
+delay(300);
+digitalWrite(6, LOW);
+delay(300);
+
+digitalWrite(7, HIGH);
+delay(300);
+digitalWrite(7, LOW);
+delay(300);
+digitalWrite(3, HIGH);
+delay(300);
+digitalWrite(3, LOW);
+delay(300);
+digitalWrite(3, HIGH);
+delay(300);
+digitalWrite(3, LOW);
+delay(300);
+digitalWrite(7, HIGH);
+delay(300);
+digitalWrite(7, LOW);
+delay(300);
+
+digitalWrite(3, HIGH);
+delay(100);
+digitalWrite(3, LOW);
+delay(100);
+digitalWrite(4, HIGH);
+delay(100);
+digitalWrite(4, LOW);
+delay(100);
+digitalWrite(5, HIGH);
+delay(100);
+digitalWrite(5, LOW);
+delay(100);
+digitalWrite(6, HIGH);
+delay(100);
+digitalWrite(6, LOW);
+delay(100);
+digitalWrite(7, HIGH);
+delay(100);
+digitalWrite(7, LOW);
+delay(100);
+
+digitalWrite(4, HIGH);
+delay(100);
+digitalWrite(4, LOW);
+delay(100);
+digitalWrite(4, HIGH);
+delay(100);
+digitalWrite(4, LOW);
+delay(100);
+
+digitalWrite(6, HIGH);
+delay(100);
+digitalWrite(6, LOW);
+delay(100);
+digitalWrite(6, HIGH);
+delay(100);
+digitalWrite(6, LOW);
+delay(100);
+
+digitalWrite(3, HIGH);
+delay(100);
+digitalWrite(3, LOW);
+delay(100);
+digitalWrite(3, HIGH);
+delay(100);
+digitalWrite(3, LOW);
+delay(100);
+
+digitalWrite(7, HIGH);
+delay(100);
+digitalWrite(7, LOW);
+delay(100);
+digitalWrite(7, HIGH);
+delay(100);
+digitalWrite(7, LOW);
+delay(100);
+
+digitalWrite(5, HIGH);
+delay(100);
+digitalWrite(5, LOW);
+delay(100);
+digitalWrite(5, HIGH);
+delay(100);
+digitalWrite(5, LOW);
+delay(100);
+
+digitalWrite(3, HIGH);
+delay(100);
+digitalWrite(3, LOW);
+delay(100);
+digitalWrite(4, HIGH);
+delay(100);
+digitalWrite(4, LOW);
+delay(100);
+digitalWrite(5, HIGH);
+delay(100);
+digitalWrite(5, LOW);
+delay(100);
+digitalWrite(6, HIGH);
+delay(100);
+digitalWrite(6, LOW);
+delay(100);
+digitalWrite(7, HIGH);
+delay(100);
+digitalWrite(7, LOW);
+delay(100);
+}
